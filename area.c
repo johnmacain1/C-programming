c0a17e067a4545d5bfad455c1c2c947c810e254d
johnmacain1 <johnmacain04@gmail.com>
2024-09-12 07:52:59 +0300

    area.c
    Is a program used to find the area of a rectangle

---
+++ area.c
@@ -0,0 +1,13 @@
+// find area of rectangle
+/* area of rectangle program
+Author: John Macain1
+*/
+#include <stdio.h> // scanf(), printf()
+int main()
+{
+    int a = 15;
+    int b = 30;
+    int product;
+    product = a * b;
+    printf("product of a and b %d", product);
+}
